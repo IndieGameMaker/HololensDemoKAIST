d7a6b326f2418179c0bf6d9b6ae3ca299b15d068
IndieGameMaker <myleje@gmail.com>
2021-02-04 11:52:50 +0900

    .

---
+++ Assets/02.Scripts/PointerManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Microsoft.MixedReality.Toolkit.Input;
+
+public class PointerManager : MonoBehaviour, IMixedRealityFocusHandler, IMixedRealityPointerHandler
+{
+    public void OnFocusEnter(FocusEventData eventData)
+    {
+        Debug.Log("OnFocusEnter");
+    }
+
+    public void OnFocusExit(FocusEventData eventData)
+    {
+        Debug.Log("OnFocusExit");
+    }
+
+    public void OnPointerClicked(MixedRealityPointerEventData eventData)
+    {
+        Debug.Log("OnPointerClicked");
+    }
+
+    public void OnPointerDown(MixedRealityPointerEventData eventData)
+    {
+        Debug.Log("OnPointerDown");
+    }
+
+    public void OnPointerDragged(MixedRealityPointerEventData eventData)
+    {
+        Debug.Log("OnPointerDragged");
+    }
+
+    public void OnPointerUp(MixedRealityPointerEventData eventData)
+    {
+        Debug.Log("OnPointerUp");
+    }
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
